6d9841a88927c0534a1a5c141029b4e24ee2706e
Dane Hillard <github@danehillard.com>
2019-11-26 18:22:27 -0500

    Update ch02 and ch05 with latest errata fixes

--- ch05/test_tax.py
+++ ch05/test_tax.py
@@ -1,3 +1,4 @@
+import io
 import unittest
 from unittest import mock
 
@@ -5,11 +6,17 @@ from tax import add_sales_tax
 
 
 class SalesTaxTestCase(unittest.TestCase):
-    @mock.patch('requests.get')  # <1>
-    def test_get_sales_tax_returns_proper_value_from_api(self, mock_get):  # <2>
+    @mock.patch('tax.urlopen')  # <1>
+    def test_get_sales_tax_returns_proper_value_from_api(
+            self,
+            mock_urlopen  # <2>
+    ):
         test_tax_rate = 1.06
-        tax_api_response = mock.Mock()  # <3>
-        tax_api_response.text = str(test_tax_rate)  # <4>
-        mock_get.return_value = tax_api_response  # <5>
+        mock_urlopen.return_value = io.BytesIO(  # <3>
+            str(test_tax_rate).encode('utf-8')
+        )
 
-        self.assertEqual(5 * test_tax_rate, add_sales_tax(5, 'USA', 'MI'))  # <6>
+        self.assertEqual(
+            5 * test_tax_rate,
+            add_sales_tax(5, 'USA', 'MI')
+        )  # <4>
